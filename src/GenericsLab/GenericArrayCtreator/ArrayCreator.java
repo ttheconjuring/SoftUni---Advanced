35bb4d3f9cacefdc146419399feded25d0fecaf8
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-06-02 22:03:13 +0300

    Initial Commit

---
+++ src/GenericsLab/GenericArrayCtreator/ArrayCreator.java
@@ -0,0 +1,23 @@
+package GenericsLab.GenericArrayCtreator;
+
+import java.lang.reflect.Array;
+
+public class ArrayCreator {
+
+    public static <T> T[] create(int length, T item) {
+        T[] array = (T[]) new Object[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = item;
+        }
+        return array;
+    }
+
+    public static <T> T[] create(Class<T> cl, int length, T item) {
+        T[] array = (T[]) Array.newInstance(cl, length);
+        for (int i = 0; i < length; i++) {
+            array[i] = item;
+        }
+        return array;
+    }
+
+}
---
+++ src/GenericsLab/GenericArrayCtreator/Main.java
@@ -0,0 +1,10 @@
+package GenericsLab.GenericArrayCtreator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] strings = ArrayCreator.create(10, "none");
+        Integer[] integers = ArrayCreator.create(Integer.class, 10, 0);
+    }
+
+}

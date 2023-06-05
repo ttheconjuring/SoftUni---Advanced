8fd2c00b9ebfadf3d17466e709e99164577bdbe2
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-06-05 21:51:33 +0300

    Initial Commit

---
+++ src/GenericsExercise/GenericSwapMethodInteger/Box.java
@@ -0,0 +1,24 @@
+package GenericsExercise.GenericSwapMethodInteger;
+
+public class Box<T> {
+
+    private T value;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Box(T value) {
+        setValue(value);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", getValue().getClass().getName(), getValue());
+    }
+
+}
---
+++ src/GenericsExercise/GenericSwapMethodInteger/Main.java
@@ -0,0 +1,47 @@
+package GenericsExercise.GenericSwapMethodInteger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(bufferedReader.readLine());
+
+            List<Box<Integer>> boxes = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                Box<Integer> box = new Box<>(Integer.parseInt(bufferedReader.readLine()));
+                boxes.add(box);
+            }
+
+            int[] indexes = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            swap(boxes, firstIndex, secondIndex);
+            boxes.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static <T> void swap(List<T> arrayList, int firstIndex, int secondIndex) {
+        T firstElement = arrayList.get(firstIndex);
+        T secondElement = arrayList.get(secondIndex);
+        arrayList.set(firstIndex, secondElement);
+        arrayList.set(secondIndex, firstElement);
+    }
+
+}
+
+

531520dd419ab53270cb33cabfcf0ca63dbcdcfd
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-06-06 22:08:50 +0300

    Initial Commit

---
+++ src/IteratorsAndComparators/Book/Book.java
@@ -0,0 +1,42 @@
+package IteratorsAndComparators.Book;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public Book(String title, int year, String... authors) {
+        setTitle(title);
+        setYear(year);
+        setAuthors(Arrays.asList(authors));
+    }
+
+}

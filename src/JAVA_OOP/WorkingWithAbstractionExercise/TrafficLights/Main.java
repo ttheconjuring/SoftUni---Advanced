d65023ccb4375ee6a3e573019e87a66a694c73e4
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-06-22 23:33:47 +0300

    Initial Commit

--- src/JAVA_OOP/WorkingWithAbstractionExercise/TrafficLights/Main.java
+++ src/JAVA_OOP/WorkingWithAbstractionExercise/TrafficLights/Main.java
@@ -3,23 +3,20 @@ package JAVA_OOP.WotkingWithAbstractionExercise.TrafficLights;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            String[] trafficLightsStringArray = bufferedReader.readLine().split(" ");
-            TrafficLights[] trafficLights = new TrafficLights[trafficLightsStringArray.length];
-            for (int i = 0; i < trafficLightsStringArray.length; i++) {
-                trafficLights[i] = new TrafficLights(Colors.valueOf(trafficLightsStringArray[i]));
-            }
+            TrafficLights[] trafficLights = Arrays.stream(bufferedReader.readLine().split(" ")).map(color -> new TrafficLights(Colors.valueOf(color))).toArray(TrafficLights[]::new);
             int n = Integer.parseInt(bufferedReader.readLine());
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < trafficLights.length; j++) {
-                    trafficLights[j].update();
-                    System.out.print(trafficLights[j].getCurrentColor() + " ");
-                }
+                Arrays.stream(trafficLights).forEach(trafficLight -> {
+                    trafficLight.update();
+                    System.out.print(trafficLight.getCurrentColor() + " ");
+                });
                 System.out.println();
             }
         } catch (IOException e) {

ac5fba1e194bd74613148276cfe6ddb2399607cf
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-06-23 21:55:39 +0300

    Initial Commit

---
+++ src/JAVA_OOP/EncapsulationLab/SortByNameAndAge/Person.java
@@ -0,0 +1,44 @@
+package JAVA_OOP.EncapsulationLab.SortByNameAndAge;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s is %d years old.", this.getFirstName(), this.getLastName(), this.getAge());
+    }
+
+}

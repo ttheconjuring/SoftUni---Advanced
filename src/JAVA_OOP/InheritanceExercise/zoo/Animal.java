15508dd3135c0261679454d69ccfd0518cd3b0ef
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-06-30 08:43:59 +0300

    Initial Commit

---
+++ src/JAVA_OOP/InheritanceExercise/zoo/Animal.java
@@ -0,0 +1,18 @@
+package zoo;
+
+public class Animal {
+
+    private String name;
+
+    public Animal(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+}

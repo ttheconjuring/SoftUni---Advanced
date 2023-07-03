2d27c4f5a4ceb7326e9e0b6cb4b492bd8ff1aeb3
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-07-03 21:03:42 +0300

    Initial Commit

---
+++ src/JAVA_OOP/InterfacesAndAbstractionExercise/MultipleImplementation/Identifiable.java
@@ -0,0 +1,7 @@
+package JAVA_OOP.InterfacesAndAbstractionExercise.MultipleImplementation;
+
+public interface Identifiable {
+
+    String getId();
+
+}
---
+++ src/JAVA_OOP/InterfacesAndAbstractionExercise/MultipleImplementation/Person.java
@@ -0,0 +1,9 @@
+package JAVA_OOP.InterfacesAndAbstractionExercise.MultipleImplementation;
+
+public interface Person {
+
+    String getName();
+
+    int getAge();
+
+}

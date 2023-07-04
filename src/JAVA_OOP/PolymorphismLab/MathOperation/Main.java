76313cb5d286d6580ec34db9d06a1207af245738
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-07-04 22:31:59 +0300

    Initial Commit

--- src/JAVA_OOP/PolymorphismLab/MathOperation/Main.java
+++ src/JAVA_OOP/PolymorphismLab/MathOperation/Main.java
@@ -1,22 +1,14 @@
 package JAVA_OOP.PolymorphismLab.MathOperation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             MathOperation math = new MathOperation();
             System.out.println(math.add(2, 2));
             System.out.println(math.add(3, 3, 3));
             System.out.println(math.add(4, 4, 4, 4));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+      
     }
 
 }
